9e214b92c19f05cfe6c456ddc6c664b13074f599
Ryan1090 <201040489+Ryan1090@users.noreply.github.com>
2025-10-03 16:02:41 +0800

    a

--- github-sync-test.php
+++ github-sync-test.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: Github Sync Test
- * Description: test wordpress <- github sync works
+ * Description: test wordpress <- github sync works test
  * Author: Ryan Marrington
  * GitHub Plugin URI: https://github.com/Ryan1090/GitHub-Sync-Test
  * Primary Branch: main
