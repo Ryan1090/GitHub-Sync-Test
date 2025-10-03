36ef4fe9aa2cce7702971c40becd58b71af5a0b4
Ryan1090 <201040489+Ryan1090@users.noreply.github.com>
2025-10-03 15:36:32 +0800

    s

--- github-sync-test.php
+++ github-sync-test.php
@@ -4,26 +4,12 @@
  * Plugin Name: Github Sync Test
  * Description: test wordpress <- github sync works goog
  * Author: Ryan Marrington
- * GitHub Plugin URI: Ryan1090/GitHub-Sync-Test
+ * GitHub Plugin URI: https://github.com/Ryan1090/GitHub-Sync-Test
  * Primary Branch: main
  * Version 1.0.0
  */
 
 
-if (!defined('ABSPATH')) die('no');
-
-if (!class_exists("GithubSyncTest")) {
-
-class GithubSyncTest {
-
-    public function init(): void {
-
-    }
-}
-
-$githubSyncTest = new GithubSyncTest();
-$githubSyncTest->init();
-
-}
 
 ?>
+
